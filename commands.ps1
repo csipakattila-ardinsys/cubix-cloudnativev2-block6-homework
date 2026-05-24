c07cf413842dd81a8f4d7394ec2e5ef20b6e3a50
CSIPAK Attila <csipak.attila@ardinsys.eu>
2026-05-24 14:40:59 +0200

    Use configmap and secret.

--- commands.ps1
+++ commands.ps1
@@ -6,6 +6,10 @@ kubectl create namespace cubix-cn
 # install/upgrade the native deployment type
 #helm upgrade spring-cubix-cn spring-cubix-cn --install --namespace cubix-cn --values native.yaml
 helm upgrade appa spring-cubix-cn --install --namespace cubix-cn --values native.yaml
+
+# create secret for API key
+kubectl create secret generic hw6secret --from-literal api-key=c7e9de1e93637eb6157e3ab03698169e0fe392b17204dc42c12c2c55aa607819 --namespace cubix-cn
+
 # uninstall the helm chart
 helm uninstall appa --namespace cubix-cn
 
