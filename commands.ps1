e707eed08da8889fe4bdefee1140cdbc634486ae
CSIPAK Attila <csipak.attila@ardinsys.eu>
2026-05-24 12:56:45 +0200

    Change helm release name

--- commands.ps1
+++ commands.ps1
@@ -2,11 +2,12 @@
 kubectl create namespace cubix-cn
 
 # install/upgrade the JVM deployment type
-helm upgrade spring-cubix-cn spring-cubix-cn --install --namespace cubix-cn --values jvm.yaml
+#helm upgrade spring-cubix-cn spring-cubix-cn --install --namespace cubix-cn --values jvm.yaml
 # install/upgrade the native deployment type
-helm upgrade spring-cubix-cn spring-cubix-cn --install --namespace cubix-cn --values native.yaml
+#helm upgrade spring-cubix-cn spring-cubix-cn --install --namespace cubix-cn --values native.yaml
+helm upgrade appa spring-cubix-cn --install --namespace cubix-cn --values native.yaml
 # uninstall the helm chart
-helm uninstall spring-cubix-cn --namespace cubix-cn
+helm uninstall appa --namespace cubix-cn
 
-# cleanup
+# delete namespace
 kubectl delete namespace cubix-cn
